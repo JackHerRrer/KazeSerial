7940614200d03dee1280fbf8bc5c23c23e81e37e
JackHerRrer <>
2026-06-12 14:25:50 +0200

    Fix application start

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  reactStrictMode: false
+  reactStrictMode: false,
+  turbopack: {
+    root: __dirname,
+  },
 };
 export default nextConfig;
 /*
